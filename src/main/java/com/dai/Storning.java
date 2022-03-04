bfa7d8a76d7f3e2e6431d481d9587b7cf474d927
Chris Dai <yuhaodai20030404@gmail.com>
2022-03-04 15:28:21 +0800

    Storing grade

---
+++ src/main/java/com/dai/Storning.java
@@ -0,0 +1,12 @@
+package com.dai;
+
+public class Storning {
+    public static void main(String[] args) {
+        String name[]={"Andy","Nancy","Chris","Jhon","Frank"};
+        int math[] ={98,87,76,65,54};
+        int english[]={56,67,78,89,90};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name[i]+"\t"+math[i]+"\t"+english[i]+"\t"+(math[i]+english[i])/2);
+        }
+    }
+}
