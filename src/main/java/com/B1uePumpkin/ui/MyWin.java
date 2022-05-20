3357f8856ed2cea4e02c0b6d11a5c1c5427483c8
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2022-05-20 16:13:18 +0800

    ActionListener

--- src/main/java/com/B1uePumpkin/ui/MyWin.java
+++ src/main/java/com/B1uePumpkin/ui/MyWin.java
@@ -1,14 +1,32 @@
 package com.B1uePumpkin.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.rmi.MarshalException;
 
 public class MyWin {
     public static void main(String[] args) {
         //swing, SWT, JavaFX
         //eXtension
         JFrame frame = new JFrame();
-        frame.setVisible(true);
+        frame.setSize(600,400);
+        frame.setLocation(300,200);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        //關掉視窗後清理資源
 
+        //add component
+        JButton button = new JButton("ok");
+
+        //匿名類別, Anonymous class
+        MyActionListener listener = new MyActionListener();
+        button.addActionListener(listener);
+
+        //layout
+        //frame.setLayout(new BorderLayout());
+        frame.setLayout(new FlowLayout());
+        frame.add(button);
+        frame.add(new JButton("Testing"));
+        frame.setVisible(true);
         System.out.println("END?");
     }
 }
