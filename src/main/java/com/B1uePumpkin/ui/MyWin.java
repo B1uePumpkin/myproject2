a6f2c79e8a63e091cd686590c1708e2321226c4a
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2022-05-20 16:31:26 +0800

    Anonymous class

--- src/main/java/com/B1uePumpkin/ui/MyWin.java
+++ src/main/java/com/B1uePumpkin/ui/MyWin.java
@@ -2,6 +2,8 @@ package com.B1uePumpkin.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.rmi.MarshalException;
 
 public class MyWin {
@@ -18,8 +20,20 @@ public class MyWin {
         JButton button = new JButton("ok");
 
         //匿名類別, Anonymous class
-        MyActionListener listener = new MyActionListener();
-        button.addActionListener(listener);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hello");
+            }
+        });
+//        button.addActionListener(new ActionListener(){
+//            public void actionPerformed(ActionEvent e){
+//                System.out.println("hahahaha");
+//            }
+//        });
+
+//        MyActionListener listener = new MyActionListener();
+//        button.addActionListener(listener);
 
         //layout
         //frame.setLayout(new BorderLayout());
