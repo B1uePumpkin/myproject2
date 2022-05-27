669937a121e0755f59d434f9b82d3e53ac23a701
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2022-05-27 15:28:45 +0800

    GUI test

---
+++ src/main/java/com/B1uePumpkin/ui/GuessFrame.java
@@ -0,0 +1,47 @@
+package com.B1uePumpkin.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GuessFrame extends JFrame {
+    //Field
+    //Structure
+    public GuessFrame(){
+        setSize(600,400);
+        setLocation(300,200);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+
+        JButton button = new JButton("Hi");
+        JLabel label=new JLabel("zzz...");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hello!");
+                label.setText("Hello!");
+            }
+        });
+        setLayout(new FlowLayout());
+        add(button);
+        add(label);
+        setVisible(true);
+
+    }
+    //Method
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        GuessFrame guessFrame = new GuessFrame();
+
+    }
+}
---
+++ src/main/java/com/B1uePumpkin/ui/MyForm.java
@@ -0,0 +1,9 @@
+package com.B1uePumpkin.ui;
+
+import javax.swing.*;
+
+public class MyForm {
+    private JButton helloButton;
+    private JPanel panel1;
+    private JTable table1;
+}
